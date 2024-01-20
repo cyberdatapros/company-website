03d6d1446fe8f0e3ecf0d32fc635f6bcea3f617a
Jenner Thomas <jennerthomas@Jenners-Air.attlocal.net>
2024-01-19 23:45:20 -0500

    dash board services

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Jumbotron from "@/components/dashboard/Jumbotron";
+import ServicesSectionDashboard from "@/components/dashboard/ServicesSection";
 import PageWrapper from "@/components/wrappers/PageWrapper";
 
 export default function Home() {
   return (
     <PageWrapper>
       <Jumbotron />
+      <ServicesSectionDashboard />
     </PageWrapper>
   );
 }
