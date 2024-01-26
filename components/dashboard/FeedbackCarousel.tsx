33e3a42fa03c8bb2f41f9f753248c488de5e97df
Jenner Thomas <jennerthomas@Jenners-Air.attlocal.net>
2024-01-26 15:28:33 -0500

    faq and feedback section

---
+++ components/dashboard/FeedbackCarousel.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import CarouselWrapper from "../wrappers/CarouselWrapper";
+import feedbackArray from "@/data/feedback";
+import styles from "@/css/feedback.module.css";
+
+const FeedbackCard = ({
+  text,
+  name,
+  title,
+}: {
+  text: string;
+  name: string;
+  title: string;
+}) => {
+  return (
+    <div className={styles["feedback-item"]}>
+      <p className={styles["feedback-text"]}>{text}</p>
+      <p className={styles["feedback-client-name"]}>{name}</p>
+      <p className={styles["feedback-client-title"]}>{title}</p>
+    </div>
+  );
+};
+
+const FeedbackCarousel = () => {
+  return (
+    <CarouselWrapper
+      header="Client Feedback"
+      subheader="What our happy clients say"
+      maxWidth={450}
+      style={{
+        backgroundColor: "#11131F",
+        borderTop: "5px solid #b58500",
+        borderBottom: "5px solid #b58500",
+        height: 561,
+      }}
+      settings={{
+        arrows: true,
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+      }}
+    >
+      {feedbackArray.map(({ feedbacktext, clientname, clienttitle }, index) => (
+        <FeedbackCard
+          key={index}
+          text={feedbacktext}
+          name={clientname}
+          title={clienttitle}
+        />
+      ))}
+    </CarouselWrapper>
+  );
+};
+
+export default FeedbackCarousel;
