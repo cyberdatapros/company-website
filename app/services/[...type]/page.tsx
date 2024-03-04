5d2808d1cc8ecdc5bfa276b8e41396261c64bdf3
Jenner Thomas <jennerthomas@Jenners-MacBook-Air.local>
2024-03-04 16:02:57 -0500

    services page seo

--- app/services/[...type]/page.tsx
+++ app/services/[...type]/page.tsx
@@ -1,8 +1,40 @@
-import ServicePage from "@/components/pages/ServicePage";
+import ServicePage, { ServiceKeyType } from "@/components/pages/ServicePage";
 import ServiceSegmentPage from "@/components/pages/ServiceSegmentPage";
 import { servicesData } from "@/data/servicesPageData";
+import { Metadata, ResolvingMetadata } from "next";
 import { redirect } from "next/navigation";
 import React from "react";
+
+type Props = {
+  params: { type: Array<string> };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const [serviceKey, segmentKey] = params.type;
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousOpenGraph = (await parent).openGraph;
+  const pageSegement = servicesData[serviceKey as ServiceKeyType].segments.find(
+    (segment) => segment.url === segmentKey
+  );
+
+  const title = pageSegement?.title.replace(/(<([^>]+)>)/gi, "");
+  const description = pageSegement?.headLine.replace(/(<([^>]+)>)/gi, "");
+  return {
+    title: `CyberData Pros ${title} Service`,
+    description: description,
+    openGraph: {
+      title: `CyberData Pros ${title} Service`,
+      description: description,
+      url: `https://cyberdatapros.com/${serviceKey}/${segmentKey}`,
+    },
+  };
+}
 
 const page = async ({ params }: { params: { type: Array<string> } }) => {
   const [service, segment] = params.type;
